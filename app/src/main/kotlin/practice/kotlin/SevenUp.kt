47168a2998449e773292ac3731bc58235a8b2e33
Tommi <tommi-lew@users.noreply.github.com>
2022-03-30 15:49:36 +0800

    doc: describe the Seven Up game

--- app/src/main/kotlin/practice/kotlin/SevenUp.kt
+++ app/src/main/kotlin/practice/kotlin/SevenUp.kt
@@ -2,6 +2,17 @@ package practice.kotlin
 
 import java.util.SortedMap
 
+/**
+ * SevenUp is a derived from a party game with the same name.
+ * Players usually sit in a circle. This is a game where
+ * participants start counting from 0. The number increment
+ * by 1 for each subsequent person. If the number contains
+ * the number 7, or is a multiple or 7, or both, the person
+ * has to say "7-up", instead of the number. If the number
+ * does not satisfy any of the above condition, all they
+ * have to do is to say the number accordingly.
+ *
+ */
 class SevenUp(
     private val startingNumber: Int = 0,
     private val lastNumber: Int = 100
